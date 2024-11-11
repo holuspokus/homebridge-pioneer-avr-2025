66cb6d99f84f0b6308da2e7699e4f6adf6858549
holuspokus <rafi@holeiter.ch>
2024-11-11 18:31:21 +0100

    dev

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function pioneerAvrAccessory(log, config) {
 
     log.debug("Preferences directory : %s", this.prefsDir);
     this.manufacturer = "Pioneer";
-    this.version = "0.1.3";
+    this.version = "0.1.4";
 
     // check if prefs directory ends with a /, if not then add it
     if (this.prefsDir.endsWith("/") === false) {
